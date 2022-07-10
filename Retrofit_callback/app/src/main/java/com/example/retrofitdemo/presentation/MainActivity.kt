50dde28c4f26215883fb1e9c583c3859714e4ab0
roberto.magno <roberto.magno@g.globo>
2022-07-10 18:33:57 -0300

    Iniciando projeto retrofit callback

---
+++ Retrofit_callback/app/src/main/java/com/example/retrofitdemo/presentation/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.retrofitdemo.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofitdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by inject()
+    private lateinit var adapter: MainAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MainAdapter()
+        binding.mainRecyclerView.adapter = adapter
+        getAlbums()
+        observe()
+    }
+
+    private fun getAlbums() {
+        CoroutineScope(Dispatchers.IO).launch {
+            viewModel.getAlbums()
+        }
+    }
+
+    private fun observe() {
+        binding.mainProgressBar.visibility = View.VISIBLE
+        viewModel.albums.observe(this) {
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.mainProgressBar.visibility = View.GONE
+            } else {
+                binding.mainProgressBar.visibility = View.GONE
+            }
+        }
+    }
+}
